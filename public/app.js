ace711a78726f9eea6ae879cf28efa131348747a
Joe Marzullo <jmarzu13@gmail.com>
2017-05-24 13:04:49 -0700

    route/controller/view for main, contact, and art sections

--- public/app.js
+++ public/app.js
@@ -1,6 +1,30 @@
-var app = angular.module('ruthCollinsArtist', ['ui-router'])
+var app = angular.module('ruthCollinsArtist', ['ui.router', 'ngAnimate']);
 
+	console.log('Ruth Collins App Module')
 
+	app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
+		function($stateProvider, $urlRouterProvider, $locationProvider) {	
+		  	$urlRouterProvider.otherwise('/');
 
-console.log('Ruth Collins App Module');
+			$stateProvider
+				.state('main', {
+					url: '/main',
+					controller: 'MainCtrl',
+					controllerAs: 'vm',
+					templateUrl: 'views/main.html'
+				})
+				.state('art', {
+					url: '/art',
+					controller: 'ArtCtrl',
+					controllerAs: 'vm',
+					templateUrl: 'views/art.html'
+				})
+				.state('contact', {
+					url: '/contact',
+					controller: 'ContactCtrl',
+					controllerAs: 'vm',
+					templateUrl: 'views/contact.html'
+				});
+	}]);			
+
 
