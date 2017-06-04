3981f16e87a87debd76c2a58fbfaf7ecdf304d49
Joe Marzullo <jmarzu13@gmail.com>
2017-06-04 10:26:34 -0700

    added parallax to the template, took away angular, it is not needed

--- public/controllers/main.js
+++
@@ -1,7 +0,0 @@
-app.controller('MainCtrl', ['$scope', function($scope) {
-  console.log('main controller');
-
-  var vm = this;
-  vm.name = 'Ruth Collins, this is working';
-  vm.math = 2 + 2;
-}]);
---
+++ public/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+  console.log('This is JavaScript, no this is Patrick');
+
+  $('.parallax').parallax();
+});
