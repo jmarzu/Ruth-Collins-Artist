8cb825b9f348da6a9686edcc65ca03ac89aa7b32
Joe Marzullo <jmarzu13@gmail.com>
2017-09-10 20:44:02 -0700

    form submission works, style at 95% WIP about me, need artist bio

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('morgan')('dev'));
 
-// Adding bootstrap
+// Bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
