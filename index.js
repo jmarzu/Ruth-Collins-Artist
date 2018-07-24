b8212dc3d32b4e5c829234226cb2405a1fb2587d
Joe Marzullo <jmarzu13@gmail.com>
2018-07-23 18:28:17 -0700

    adding Procfile

--- index.js
+++ index.js
@@ -91,6 +91,9 @@ app.post('/contact', function(req, res) {
 //     });
 // });
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, function () {
+	  var port = server.address().port;
+	  console.log("Express is working on port " + port);
+}
 
 module.exports = server;
